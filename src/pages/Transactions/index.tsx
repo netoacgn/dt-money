8c63037f4f7c91781c05caec23dc01bbeba87d9e
Antonio <acgomesn@gmail.com>
2023-04-14 11:45:07 -0300

    componente Summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,12 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 
 export function Transactions() {
     return (
         <div>
             <Header />
+
+            <Summary />
         </div>
     )
 }

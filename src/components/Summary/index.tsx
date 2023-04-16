973d6895d29c3b232823ce58aa01af632e898f63
Antonio <acgomesn@gmail.com>
2023-04-16 10:43:38 -0300

    criando hook personalizado para o summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -3,29 +3,11 @@ import { SummaryCard, SummaryContainer } from "./styles";
 import { useContext } from "react";
 import { TransactionsContext } from "../../contexts/Transactions";
 import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary() {
-    const { transactions } = useContext(TransactionsContext);
-
-    const summary = transactions.reduce(
-        (accumulator, transaction) => {
-            if (transaction.type === 'income') {
-                accumulator.income += transaction.price;
-                accumulator.total += transaction.price;
-            } else {
-                accumulator.outcome += transaction.price;
-                accumulator.total -= transaction.price;
-            }
-
-            return accumulator;
-        },
-        {
-            income: 0,
-            outcome: 0,
-            total: 0,
-        }
-    );
-
+    const summary = useSummary();
+    
     return (
         <SummaryContainer>
             <SummaryCard>
---
+++ src/hooks/useSummary.ts
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { TransactionsContext } from "../contexts/Transactions";
+
+export function useSummary () {
+    const { transactions } = useContext(TransactionsContext);
+
+    const summary = transactions.reduce(
+        (accumulator, transaction) => {
+            if (transaction.type === 'income') {
+                accumulator.income += transaction.price;
+                accumulator.total += transaction.price;
+            } else {
+                accumulator.outcome += transaction.price;
+                accumulator.total -= transaction.price;
+            }
+
+            return accumulator;
+        },
+        {
+            income: 0,
+            outcome: 0,
+            total: 0,
+        }
+    );
+
+    return summary;
+}
